183b2708b2d9cdd1e75846758c8fabc1594d2606
Ressius <tonydumu@hotmail.com>
2017-04-23 21:11:58 -0400

    ajouts pour new calls

--- Equipe.go
+++ Equipe.go
@@ -15,3 +15,8 @@ type Equipe struct {
 	Entraineurs []Entraineur `gorm:"many2many:entraineur_equipe;"`
 	Joueurs     []Joueur     `gorm:"many2many:joueur_equipe;"`
 }
+
+// Expand ajoute les joueurs de l'équipe
+func (p *Equipe) Expand(db *gorm.DB) {
+	db.Model(p).Related(&(p.Joueurs), "Joueurs")
+}
